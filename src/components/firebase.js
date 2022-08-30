35f181130e1cd879370d22e4819b9cdd47dd4e98
adnan2212 <73057698+adnan2212@users.noreply.github.com>
2022-08-30 15:21:58 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,7 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAgmpmr3ytdoy3oNYc1wzXeEjQoW7V_XmY",
-  authDomain: "clone-a030c.firebaseapp.com",
-  projectId: "clone-a030c",
-  storageBucket: "clone-a030c.appspot.com",
-  messagingSenderId: "98914052925",
-  appId: "1:98914052925:web:d4ccc6e7e9546bcbaa3354",
-  measurementId: "G-255Z438MS1",
+ ...
 };
 
 // eslint-disable-next-line no-unused-vars
